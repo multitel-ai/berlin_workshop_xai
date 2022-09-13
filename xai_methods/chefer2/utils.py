74a93b302131ade08f6757eb2ebf6bd8e150eb1d
Alexandre Englebert <alexandre.englebertdenis@gmail.com>
2022-09-13 08:43:29 +0200

    xai_methods: chefer2: Add recursive setattr/getattr

---
+++ xai_methods/chefer2/utils.py
@@ -0,0 +1,14 @@
+import functools
+
+
+# Nested setattr and getattr from https://stackoverflow.com/questions/31174295/getattr-and-setattr-on-nested-objects/31174427?noredirect=1#comment86638618_31174427
+def rsetattr(obj, attr, val):
+    pre, _, post = attr.rpartition('.')
+    return setattr(rgetattr(obj, pre) if pre else obj, post, val)
+
+
+def rgetattr(obj, attr, *args):
+    def _getattr(obj, attr):
+        return getattr(obj, attr, *args)
+
+    return functools.reduce(_getattr, [obj] + attr.split('.'))
