80cc4059b05346cdb028e0a6f2e99e602e977719
Alexandre Englebert <alexandre.englebertdenis@gmail.com>
2022-09-14 09:05:58 +0200

    backend: More verbose comments

--- visualisation/backend/run.py
+++ visualisation/backend/run.py
@@ -14,49 +14,66 @@ from xai_methods.chefer2 import wrap_transformer
 
 app = Flask(__name__)
 
+# Define the model for clip
 model_type = "ViT-B/32"
+# The patch size must correspond to the model size
 patch_size = 32
 
+# Load the model and send it to gpu if available
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 model, preprocess = clip.load(model_type)
 model.to(device)
 
+
+# Requests sent to the /attentions endpoints return the attention weights
 @app.route('/attentions', methods=['POST'])
 def attentions():
+
+    # Check if 'Content-Type is json
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         data = json.loads(request.data)
     else:
         return 'Content-Type not supported!'
 
+    # Download the image from the given url and convert it to a Image class
     image_url = data['img_url']
     r = requests.get(image_url)
     image = Image.open(io.BytesIO(r.content))
+
+    # Get the text
     text = data['text']
 
     input_resolution = model.visual.input_resolution
     context_length = model.context_length
     vocab_size = model.vocab_size
 
+    # Wrap the visual and text parts of clip to get the attentions weights
     visual_transformer = wrap_transformer(model.visual)
     text_transformer = wrap_transformer(model.transformer)
 
+    # Tokenize the text
     tokens = clip.tokenize(text).to(device)
+
+    # Preprocess the image into a tensor with the right dimensions
     image = preprocess(image).to(device).unsqueeze(0)
 
+    # Compute the dimensions of the image tokens based on the image size and patch size
     image_tokens_size = torch.tensor(image.shape[-2:]) // patch_size
 
+    # Compute the coordinates of the image tokens
     y_size = image_tokens_size[0].item()
     x_size = image_tokens_size[1].item()
     y = torch.tensor(range(y_size)).unsqueeze(1)
     x = torch.tensor(range(x_size)).unsqueeze(0)
-
     coord_tensor = torch.stack((y.repeat((1, y_size)), x.repeat((x_size, 1)))) * patch_size
     img_coords = coord_tensor.flatten(1).T.numpy().tolist()
 
+    # Compute the embeddings for the image and the text
     image_embedding = visual_transformer(image.type(model.dtype))
     text_embedding = model.encode_text(tokens)
 
+    # Retrieve the attentions weights for image and text
     image_attentions = visual_transformer.attention_weights
     text_attentions = text_transformer.attention_weights
 
@@ -64,7 +81,7 @@ def attentions():
     image_attentions = torch.cat([attn[1].detach().cpu() for attn in list(image_attentions.items())])
     text_attentions = torch.cat([attn[1].detach().cpu() for attn in list(text_attentions.items())])
 
-
+    # Send the response in a json
     response = jsonify({'img_emb': image_embedding.detach().cpu().numpy().tolist(),
                         'txt_emb': text_embedding.detach().cpu().numpy().tolist(),
                         'image_attention': image_attentions.numpy().tolist(),
