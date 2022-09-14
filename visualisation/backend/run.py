af55fdde7d5e2bd6aa90e4338cb785cbc25912e0
jebogaert <jeremie.bogaert@student.uclouvain.be>
2022-09-14 11:48:28 +0200

    Fixing import name

--- visualisation/backend/run.py
+++ visualisation/backend/run.py
@@ -11,7 +11,7 @@ import numpy as np
 from CLIP.clip import clip
 
 from xai_methods.chefer2 import wrap_transformer
-from xai_methods.chefer2 import salience
+from xai_methods.chefer2 import chefer2_salience
 
 app = Flask(__name__)
 
