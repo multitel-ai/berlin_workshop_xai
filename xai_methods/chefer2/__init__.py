4e9f4fa00ce939f31b115140e25fb16b181eda53
altares <altares@multitel.be>
2022-10-19 13:48:22 +0200

    fix import for new modular saliency function

--- xai_methods/chefer2/__init__.py
+++ xai_methods/chefer2/__init__.py
@@ -1,2 +1,3 @@
 from .wrapper import wrap_transformer, WrappedMultiheadAttention
-from .saliency import salience as chefer2_saliency
+# from .saliency import salience as chefer2_saliency
+from .saliency import salience_modular as chefer2_saliency
